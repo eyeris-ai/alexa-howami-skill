abd1018677d3ed4236954493a3933545179620e6
David J. Kordsmeier <dkords@iotone.io>
2017-01-06 04:51:38 -0800

    Fix typos in the Intent naming.  Document script to package.

--- alexa-howami-skill-js/scripts/package-lambda-function.sh
+++ alexa-howami-skill-js/scripts/package-lambda-function.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd src
+# NOTE: If you have any native modules, you MUST package them up
+# of course, it must match the architecture of the AWS EC2/Lambda
 zip archive.zip AlexaSkill.js index.js package.json config.json
