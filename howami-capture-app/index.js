cf4eb8871207e05c9cc4c3be4b06d94340e1c533
David J. Kordsmeier <dkords@iotone.io>
2017-01-06 00:22:08 -0800

    Get the inital working communication with EmoVu Web API

--- howami-capture-app/index.js
+++ howami-capture-app/index.js
@@ -8,11 +8,13 @@ var querystring = require('querystring');
 var v4l2camera = require("v4l2camera");
 var pngjs = require("pngjs");
 var FormData = require('form-data');
+var request = require('request');
 
 var config = require("./config.json");
 
-var EMOVU_WEB_API_BASE_URL = "http://api.emovu.com/api/imageframe/";
-var PNGOUT = "capture.png";
+var EMOVU_WEB_API_BASE_URL = "http://api.emovu.com/api/imageframe";
+var PNGOUT = "capture";
+var MAX_FRAMES = 30;
 var frameno = 0;
 var lastFrameResult = undefined;
 var starttimeinms = 0;
@@ -29,6 +31,7 @@ var server = http.createServer(function (req, res) {
             "<script>(", pngLoaderScript.toString(), ")()</script>",
             "</head><body>",
             "<img id='cam' width='352' height='288' />",
+            "<EM>TODO: Fix web viewer, truncation</EM>",
             "</body></html>",
         ].join(""));
         return;
@@ -39,16 +42,21 @@ var server = http.createServer(function (req, res) {
             "cache-control": "no-cache",
         });
         var png = toPng();
-        frameno++;
+        
         // XXX Add in some code to compute FPS
-        console.log("Frame #" + frameno);
+        console.log("Viewing Frame #" + frameno);
+
         // Call EmoVu API on every frame
-        analyzeFaceFrame(png, lastFrameResult, function(data){
+        /* analyzeFaceFrame(png, lastFrameResult, function(data){
         	if (data) {
         		lastFrameResult = data;
         	}
         });
-        return png.pack().pipe(res);
+        */
+        // png.pack().pipe(fs.createWriteStream(PNGOUT));
+        // return png.pack().pipe(res);
+        var fileStream = fs.createReadStream(__dirname + '/' + PNGOUT + "-" + frameno + ".png");
+        return fileStream.pipe(res);
     }
 });
 server.listen(3000);
@@ -69,11 +77,43 @@ var pngLoaderScript = function () {
     }, false);
 };
 
+var pngLoaderScript2 = function() {
+    setInterval(function() {
+	   	window.addEventListener("load", function (ev) {
+		    
+		    	console.log("Reloading");
+		    	var img = new Image();
+		    	var cam = document.getElementById("cam");
+		    	cam.parentNode.replaceChild(img, cam);
+		    	img.src = "/" + Date.now() + ".png";
+		}, false);
+	}, 1000);
+};
+
+var pngLoaderScript3 = function () {
+    window.addEventListener("load", function (ev) {
+        var cam = document.getElementById("cam");
+        (function load() {
+            var img = new Image();
+            img.addEventListener("load", function loaded(ev) {
+                cam.parentNode.replaceChild(img, cam);
+                img.id = "cam";
+                cam = img;
+                setTimeout(function() {
+                	load();
+                }, 1000);
+            }, false);
+            img.src = "/" + Date.now() + ".png";
+        })();
+    }, false);
+};
+
 var toPng = function () {
+	// console.log("toPng:", new Date());
     var rgb = cam.toRGB();
     var png = new pngjs.PNG({
-        width: cam.width, height: cam.height,
-        deflateLevel: 1, deflateStrategy: 1,
+        width: cam.width, height: cam.height, filterType: -1,
+        deflateLevel: 9, deflateStrategy: 3,
     });
     var size = cam.width * cam.height;
     for (var i = 0; i < size; i++) {
@@ -82,19 +122,59 @@ var toPng = function () {
         png.data[i * 4 + 2] = rgb[i * 3 + 2];
         png.data[i * 4 + 3] = 255;
     }
-    png.pack().pipe(fs.createWriteStream(PNGOUT));
+    
+    png.pack().pipe(fs.createWriteStream(PNGOUT + "-" + frameno + ".png"));
+    // Call EmoVu API on every frame
+    console.log("Writing " + PNGOUT + "-" + frameno + ".png");
+
+    /*
+    if (frameno > 5) {
+    analyzeFaceFrame(frameno-5, lastFrameResult, function(data){
+    	if (data) {
+    		lastFrameResult = data;
+    		console.log("Received result: ", data);
+    	}
+    });
+    }
+    */
+
+    /*
+    analyzeFaceFrame(frameno, lastFrameResult, function(data){
+        if (data) {
+            lastFrameResult = data;
+            console.log("Received result: ", data);
+        }
+    });
+    */
+    // if (frameno === 5 || frameno === 10 || frameno === 15 || frameno === 20 || frameno === 25 || frameno === 30) {
+    if (frameno === 10 || frameno === 20 || frameno === 30) {
+    analyzeFaceFrame(frameno-1, lastFrameResult, function(data){
+        if (data) {
+            lastFrameResult = data;
+            console.log("Received result: ", data);
+        }
+    });
+    }
+
+    if (frameno === MAX_FRAMES) {
+    	frameno = 0;
+    } else {
+    	frameno++;
+    }
     return png;
 };
 
-var analyzeFaceFrame = function(png, lastFrameResult, callback) {
+var analyzeFaceFrame = function(pngframeno, lastFrameResult, callback) {
 	console.log("analyzeFaceFrame");
 	var result = undefined;
 
+	var timestamp;
 	if (starttimeinms === 0) {
 		starttimeinms = Date.now();
+		timestamp = 0;
+	} else {
+		timestamp = Date.now()-starttimeinms;
 	}
-
-	var timestamp = Date.now()-starttimeinms;
 	/*
 		An EmoVu Web API call requires these params:
 
@@ -106,85 +186,40 @@ var analyzeFaceFrame = function(png, lastFrameResult, callback) {
 		Further tuning params:
 		XXX TODO
 	*/
-	var postdata = querystring.stringify({
-		"LicenseKey": config.emovu_api_key,
-		"previousFrameResult": lastFrameResult,
-		"imageFile": png,
-		"timestamp": timestamp
-	});
-
 	if (!lastFrameResult) {
 		lastFrameResult = "";
 	}
-	var form = new FormData();
-	
-	// form.append('my_buffer', new Buffer(10));
-	// form.append('my_file', fs.createReadStream('/foo/bar.jpg'));
 
-	form.append('LicenseKey', config.emovu_api_key);
-	form.append('previousFrameResult', lastFrameResult);
-	form.append('imageFile', PNGOUT);
-	form.append('timestamp', timestamp);
-	// console.log(form);
-
-	var request = http.request({
-	  method: 'post',
-	  host: 'api.emovu.com',
-	  path: '/api/imageframe',
-	  headers: form.getHeaders()
+    console.log("Read stream for: " + __dirname + '/' + PNGOUT + "-" + pngframeno + ".png");
+	var formData = {
+	  // Pass a simple key-value pair
+	  timestamp: timestamp,
+	  previousFrameResult: lastFrameResult,
+	  imageFile: fs.createReadStream(__dirname + '/' + PNGOUT + "-" + pngframeno + ".png")
+      // imageFile: fs.createReadStream(__dirname + '/capture-99.png')
+	};
+	// console.log("Calling emovu with formdata ", formData);
+	request.post({
+		url: EMOVU_WEB_API_BASE_URL,
+		formData: formData,
+		headers: {
+			'LicenseKey': config.emovu_api_key
+		}
+	}, function optionalCallback(err, httpResponse, body) {
+	  if (err) {
+	    return console.error('upload failed:', err);
+	  }
+	  console.log('Post successful for frame#: " + pngframeno + " Server responded with:', body);
+      console.log(httpResponse.statusCode);
 	});
-
-	form.pipe(request);
-
-	request.on('response', function(res) {
-	  console.log(res.statusCode);
-	  callback({});
-	});
-
-	request.end();
-
-	/*
-	var req = http.request(
-		{
-			host: 'api.emovu.com',
-            path: '/api/imageframe/',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                'Content-Length': Buffer.byteLength(postdata)
-            }
-        }, function(response) {
-        // Continuously update stream with data
-        response.setEncoding('utf-8');
-        var body = '';
-        response.on('data', function(d) {
-            body += d;
-        });
-        response.on('error', function(err) {
-      		console.error(err);
-    	});
-
-        response.on('end', function() {
-        	console.log("Results\n=======\n");
-        	console.log(body);
-            // var parsed = JSON.parse(body);
-
-            // Pass back results
-            callback({});
-        });
-    });
-	req.on('error', function() {
-		console.log("error");
-	});
-    req.write(postdata);
-    req.end();
-    */
 
 };
+
 var cam = new v4l2camera.Camera("/dev/video0");  // XXX Hardcoded values
 cam.configSet({width: 352, height: 288}); // XXX Harcoded values
 cam.start();
 cam.capture(function loop() {
+	toPng();
     cam.capture(loop);
 });
 
